69cfbd8c0e040c8dd57586a1c8b428c9fcac787c
konomu <45617438+konomu@users.noreply.github.com>
2024-08-08 05:33:14 -0700

    Update EXT_pitch_accent.lua

--- extensions/EXT_pitch_accent.lua
+++ extensions/EXT_pitch_accent.lua
@@ -1,3 +1,61 @@
+local logger = require("logger")
+local util = require("util")
+local u = require("lua_utils/utils")
+
+local PitchAccent = {
+    description = [[
+Some definitions contain pitch accent information.
+e.g. さけ・ぶ [2]【叫ぶ】
+this extension extracts the [2] from the definition's headword and stores it as a html representation and/or a number.
+    ]],
+    -- These 2 fields should be modified to point to the desired field on the card
+    field_pitch_html = 'Graph',
+    field_pitch_num = 'Graph',
+
+    -- SVG element templates used to display pitch accent
+    pitch_pattern = "<svg xmlns=\"http://www.w3.org/2000/svg\" focusable=\"false\" viewBox=\"0 0 %d 100\" style=\"display:inline-block;vertical-align:middle;height:1.5em;\">%s</svg>",
+    mark_accented = "<circle cx=\"%d\" cy=\"25\" r=\"15\" style=\"stroke-width:5;fill:currentColor;stroke:currentColor;\"></circle>",
+    mark_downstep = "<circle cx=\"%d\" cy=\"25\" r=\"15\" style=\"fill:none;stroke-width:5;stroke:currentColor;\"></circle><circle cx=\"%d\" cy=\"25\" r=\"5\" style=\"fill:currentColor;\"></circle>",
+    unmarked_char = "<circle cx=\"%d\" cy=\"75\" r=\"15\" style=\"stroke-width:5;fill:currentColor;stroke:currentColor;\"></circle>",
+    pitch_downstep_pattern = "(%[([0-9])%])",
+}
+
+function PitchAccent:convert_pitch_to_HTML(accents)
+    local converter = nil
+    if #accents == 0 then
+        converter = function(_) return nil end
+    elseif #accents == 1 then
+        converter = function(field) return accents[1][field] end
+    else
+        converter = function(field) return self:convert_to_HTML {
+            entries = accents,
+            class = "pitch",
+            build = function(accent) return string.format("<li>%s</li>", accent[field]) end
+        }
+        end
+    end
+    return converter("pitch_num"), converter("pitch_accent")
+end
+
+function PitchAccent:split_morae(word)
+    local small_aeio = u.to_set(util.splitToChars("ゅゃぃぇょゃ"))
+    local morae = u.defaultdict(function() return {} end)
+    for _,ch in ipairs(util.splitToChars(word)) do
+        local is_small = small_aeio[ch] or false
+        table.insert(morae[is_small and #morae or #morae+1], ch)
+    end
+    logger.info(("EXT: PitchAccent#split_morae(): split word %s into %d morae: "):format(word, #morae))
+    return morae
+end
+
+local function get_first_line(linestring)
+    local start_idx = linestring:find('\n', 1, true)
+    return start_idx and linestring:sub(1, start_idx + 1) or linestring
+end
+
+function PitchAccent:get_pitch_downsteps(dict_result)
+    return string.gmatch(get_first_line(dict_result.definition), self.pitch_downstep_pattern)
+end
 function PitchAccent:get_pitch_accents(dict_result)
     local _morae = nil
     local function get_morae()
@@ -86,3 +144,29 @@ function PitchAccent:get_pitch_accents(dict_result)
         end
     end, downstep_iter
 end
+
+function PitchAccent:run(note)
+    if not self.popup_dict.is_extended then
+        self.popup_dict.results = require("langsupport/ja/dictwrapper").extend_dictionaries(self.popup_dict.results, self.conf)
+        self.popup_dict.is_extended = true
+    end
+    local selected = self.popup_dict.results[self.popup_dict.dict_index]
+
+    local pitch_accents = {}
+    for _, result in ipairs(self.popup_dict.results) do
+        if selected:get_kana_words():contains_any(result:get_kana_words()) then
+            for num, accent in self:get_pitch_accents(result) do
+                if not pitch_accents[num] then
+                    pitch_accents[num] = true -- add as k/v pair too so we can detect uniqueness
+                    table.insert(pitch_accents, { pitch_num = num, pitch_accent = accent })
+                end
+            end
+        end
+    end
+    local pitch_num, pitch_accent = self:convert_pitch_to_HTML(pitch_accents)
+    note.fields[self.field_pitch_num] = pitch_num
+    note.fields[self.field_pitch_html] = pitch_accent
+    return note
+end
+
+return PitchAccent
